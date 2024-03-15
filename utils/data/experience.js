f0a418555c88803381257c7570b61d0acc530d6b
Aman Vishwakarma <vishwakarmaaman8000@gmail.com>
2024-03-15 13:32:02 +0530

    personal portfolio complete

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Software Engineer I',
-    company: "Teton Private Ltd.",
-    duration: "(Jan 2022 - Present)"
+    title: "Junior Backend Developer",
+    company: "SunoKitaab Private Ltd.",
+    duration: "(July 2023 - Present)",
   },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Fiverr (freelance)",
-    duration: "(Jun 2021 - Jan 2022)"
+    title: "Backend developer Intern",
+    company: "SunoKitaab Private Ltd.",
+    duration: "(May 2023 - June 2023)",
   },
   {
     id: 3,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)"
-  }
-]
+    duration: "(April 2023 - Present)",
+  },
+];
